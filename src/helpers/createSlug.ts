c952db4cf07062e61bb1ccbfb152dac336a4bce5
Christopher Robinson <hello@christopherrobinson.uk>
2025-04-25 22:57:14 +0100

    Add structured data to `Recipes` and improve styles

--- src/helpers/createSlug.ts
+++ src/helpers/createSlug.ts
@@ -4,9 +4,15 @@ export const createSlug = (input: string | undefined | null): string => {
   }
 
   return input
-    .trim()                         // remove leading & trailing whitespace
-    .replace(/[^A-Za-z0-9- ]/g, '') // remove special characters
-    .replace(/\s+/g, '-')           // replace spaces
-    .replace(/^-+|-+$/g, '')        // remove leading & trailing separators
-    .toLowerCase();                 // output lowercase
+    .replace(/[’‘]/g, "'")            // replace curly apostrophes
+    .replace(/[“”]/g, '"')            // replace curly quotes
+    .replace(/\s+/g, ' ')             // collapse multiple spaces
+    .trim()                           // trim whitespace
+    .normalize('NFD')                 // split accents from letters (e.g. é → e + ́)
+    .replace(/[\u0300-\u036f]/g, '')  // remove accent characters (diacritics)
+    .replace(/&/g, 'and')             // convert & to 'and'
+    .toLowerCase()                    // convert to lowercase
+    .replace(/(\w)['’]s\b/g, '$1s')   // replace curly or straight apostrophe-s (e.g. "cheat's") with just "s"
+    .replace(/[^a-z0-9]+/g, '-')      // replace non-alphanumeric characters with hyphens
+    .replace(/^-+|-+$/g, '');         // remove leading/trailing hyphens
 };
