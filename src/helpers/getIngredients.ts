4b39c87dee0a565ebb42097ba42e68501b7bd677
Christopher Robinson <hello@christopherrobinson.uk>
2025-04-29 16:28:07 +0100

    Create `getIngredients` function

---
+++ src/helpers/getIngredients.ts
@@ -0,0 +1,33 @@
+let ingredientCache: typeof recipes[0]['data']['ingredients'][0][] | null = null;
+
+export const getIngredients = async () => {
+  if (ingredientCache) {
+    return ingredientCache;
+  }
+
+  const recipes = await getRecipes();
+
+  const ingredientMap = new Map<string, typeof recipes[0]['data']['ingredients'][0]>();
+
+  recipes.forEach(({ data }) => {
+    const { ingredients, portion_sizes } = data;
+
+    if (!ingredients || !portion_sizes) return;
+
+    const portionForTwo = portion_sizes.find(p => p.portions === 2);
+    if (!portionForTwo?.ingredients_skus) return;
+
+    const validIds = new Set(portionForTwo.ingredients_skus.map(sku => sku.id));
+
+    for (let i = 0; i < ingredients.length; i++) {
+      const ingredient = ingredients[i];
+      if (validIds.has(ingredient.gousto_uuid)) {
+        ingredientMap.set(ingredient.gousto_uuid, ingredient);
+      }
+    }
+  });
+
+  ingredientCache = Array.from(ingredientMap.values());
+
+  return ingredientCache;
+};
