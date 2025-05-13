9017c41cfb5964bde5916c626f7adffc08e9d108
Christopher Robinson <hello@christopherrobinson.uk>
2025-05-13 22:19:31 +0100

    Create `Inspiration` page showing random recipes

---
+++ src/pages/api/inspiration.ts
@@ -0,0 +1,31 @@
+import type { APIRoute } from 'astro';
+
+export const prerender = false;
+
+export const GET: APIRoute = async () => {
+  try {
+    const { data: recipes } = await supabaseClient.rpc('get_random_recipes', { limit_count: 4 });
+    const recipesJSON = recipes.map(({ cuisine, image, prep_times, rating, url, title }) => ({
+      cuisine: cuisine,
+      id: url,
+      image: image,
+      prep_times: prep_times,
+      rating: rating,
+      title: title,
+    }));
+
+    return new Response(JSON.stringify(recipesJSON.slice(0, 4)), {
+      headers: {
+        'Cache-Control': 'public, max-age=604800, s-maxage=604800',
+        'Content-Type': 'application/json',
+        'Netlify-Vary': 'query',
+      },
+    });
+  }
+  catch (error) {
+    return new Response(JSON.stringify({ error: 'Failed to retrieve recipes.' }), {
+      headers: { 'Content-Type': 'application/json' },
+      status: 500,
+    });
+  }
+}
