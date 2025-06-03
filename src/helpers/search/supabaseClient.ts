4563711b70923853e227504822dd2a2d8c7d32a7
Christopher Robinson <hello@christopherrobinson.uk>
2025-06-03 21:34:01 +0100

    Swap out "Netlify" for "Cloudflare Pages"

--- src/helpers/search/supabaseClient.ts
+++ src/helpers/search/supabaseClient.ts
@@ -1,4 +1,4 @@
 import { createClient } from '@supabase/supabase-js';
-import { SUPABASE_KEY, SUPABASE_URL } from 'astro:env/client';
+import { SUPABASE_KEY, SUPABASE_URL } from 'astro:env/server';
 
 export const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY);
