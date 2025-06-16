d2807eb1a680c56feea416fffbc114a2076f368f
Christopher Robinson <hello@christopherrobinson.uk>
2025-06-16 22:46:42 +0100

    Remove unused `mapToSortedArray` function

--- src/helpers/mapToSortedArray.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Converts Map to sorted array using a comparison function
- */
-export const mapToSortedArray = <K, V>(
-  map: Map<K, V>,
-  compareFn: (a: V, b: V) => number
-): V[] => {
-  return Array.from(map.values()).sort(compareFn);
-};
