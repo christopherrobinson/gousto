c72ec51fa49b445a58c2da75136b4bec4a9c8110
Christopher Robinson <hello@christopherrobinson.uk>
2025-06-16 22:26:57 +0100

    Remove unused `getNestedProperty` function

--- src/helpers/getNestedProperty.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Gets a nested property value from an object using dot notation
- */
-export const getNestedProperty = (obj: any, path: string): any => {
-  return path.split('.').reduce((current, key) => current?.[key], obj);
-};
