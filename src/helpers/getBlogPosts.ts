2009ab222d9ce6bb2bea98a21c673ba64fb0d1d5
Christopher Robinson <hello@christopherrobinson.uk>
2025-07-08 19:23:03 +0100

    Update `getBlogPosts` function to add "limit" param

--- src/helpers/getBlogPosts.ts
+++ src/helpers/getBlogPosts.ts
@@ -1,15 +1,16 @@
+type GetBlogPostsOptions = {
+  limit?: number;
+};
+
 let blogCache: any[] | null = null;
 
-export const getBlogPosts = async (limit?: number) => {
+export const getBlogPosts = async (options: GetBlogPostsOptions = {}) => {
   if (!blogCache) {
     const blogPosts = await getCollection('blog');
 
     blogCache = blogPosts.sort((a, b) => b.data.date.valueOf() - a.data.date.valueOf());
   }
 
-  if (typeof limit === 'number' && limit > 0) {
-    return blogCache.slice(0, limit);
-  }
-
-  return blogCache;
+  // Apply limit if provided
+  return (typeof options.limit === 'number') ? blogCache?.slice(0, options.limit) : blogCache;
 };
