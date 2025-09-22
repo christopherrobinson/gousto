243b3950c7439bc48ebf728b8e5644ffe1608860
Christopher Robinson <hello@christopherrobinson.uk>
2025-09-22 18:35:25 +0100

    Update interface for `NutritionalInformationProps`

--- src/types/NutritionalInformation.ts
+++ src/types/NutritionalInformation.ts
@@ -1,5 +1,13 @@
 export interface NutritionalInformationProps {
-  perPortion: {
+  perHundredGrams?: {
+    carbs_mg?: number;
+    energy_kcal?: number;
+    fat_mg?: number;
+    fibre_mg?: number;
+    protein_mg?: number;
+    salt_mg?: number;
+  };
+  perPortion?: {
     carbs_mg?: number;
     energy_kcal?: number;
     fat_mg?: number;
